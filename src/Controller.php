a57f57ca762bf2f674785e3092f3733df66e81ec
Rahimie Ahmad <newrehmi@gmail.com>
2020-12-27 14:53:38 +0800

    make __construct final to prevent extension

--- src/Controller.php
+++ src/Controller.php
@@ -4,4 +4,8 @@ namespace Sigil;
 
 abstract class Controller extends \Exedra\Routeller\Controller\Controller
 {
+    protected final function __construct()
+    {
+        parent::__construct();
+    }
 }
