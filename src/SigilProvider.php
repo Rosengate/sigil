5cb54a188590e51730ebe309f4deb205e5b35a9d
Rahimie Ahmad <newrehmi@gmail.com>
2021-05-04 00:37:27 +0800

    welcome-sigil publish fix

--- src/SigilProvider.php
+++ src/SigilProvider.php
@@ -12,7 +12,7 @@ class SigilProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/../config/sigil.php' => config_path('sigil.php'),
-            __DIR__ . '/../views/welcome-sigil.blade.php' => resource_path('views/welcome-sigil.php'),
+            __DIR__ . '/../views/welcome-sigil.blade.php' => resource_path('views/welcome-sigil.blade.php'),
             __DIR__ . '/../app/Http/Controllers/RootController.php' => app_path('Http/Controllers/RootController.php'),
             __DIR__ . '/../app/Http/Controllers/WebController.php' => app_path('Http/Controllers/WebController.php'),
         ]);
